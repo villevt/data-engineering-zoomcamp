d0776f68b09da1b63e1c3069084b3edc2300a4a1
villevt <ville.turku99@gmail.com>
2023-07-09 00:32:30 +0300

    Fixed service account impersonation

--- cohorts/2023/week_1_terraform/terraform/provider.tf
+++ cohorts/2023/week_1_terraform/terraform/provider.tf
@@ -1,25 +1,27 @@
-data "google_secret_manager_secret_version" "terraform_service_account_email" {
- secret   = "terraform_service_account_email"
+provider "google" {
+  project = "zoomcamp-392000"
+  alias   = "impersonation"
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
 }
 
-provider "google" {
- alias = "impersonation"
- scopes = [
-   "https://www.googleapis.com/auth/cloud-platform",
-   "https://www.googleapis.com/auth/userinfo.email",
- ]
+data "google_secret_manager_secret_version" "terraform_service_account_email" {
+  provider = google.impersonation
+  secret   = "terraform_service_account_email"
 }
 
 data "google_service_account_access_token" "default" {
- provider               	= google.impersonation
- target_service_account 	= data.google_secret_manager_secret_version.terraform_service_account_email.secret_data
- scopes                 	= ["userinfo-email", "cloud-platform"]
- lifetime               	= "1200s"
+  provider               = google.impersonation
+  target_service_account = data.google_secret_manager_secret_version.terraform_service_account_email.secret_data
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "1200s"
 }
 
 provider "google" {
- project 		    = "zoomcamp-392000"
- access_token	    = data.google_service_account_access_token.default.access_token
- request_timeout    = "60s"
- zone               = "us-east1"
+  project         = "zoomcamp-392000"
+  access_token    = data.google_service_account_access_token.default.access_token
+  request_timeout = "60s"
+  zone            = "us-east1"
 }
